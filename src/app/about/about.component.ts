49da185669eed039678f693f1cd32abc7d4e79a9
MuhannadAmr <mohanedamr2015@gmail.com>
2024-02-01 22:26:37 +0200

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,141 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+
+  pro: Product[] = [{
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  },
+  {
+    pImgSrc: "../../assets/img/Samsung-Galaxy-S23.jpg",
+    pTitle: "Galaxy S23 Ultra 5G",
+    pDesc: "The use of conscious materials makes this our most eco-friendly smartphone ever. Recycled glass and PET film accent the phone's exterior, and box made from recycled paper makes it planet-loving from the moment it hits your hands.",
+    pPrice: 50000
+  }
+
+];
+
+}
+
+
+interface Product {
+  pImgSrc: string;
+  pTitle: string;
+  pDesc: string;
+  pPrice: number
+}
